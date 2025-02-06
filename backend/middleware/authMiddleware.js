eb4964140cd4b8e9ae71cce33fba9cebd85b53d0
afreen shaik <affushaik9493@gmail.com>
2025-02-07 02:07:21 +0530

    add middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.authenticate = async (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) return res.status(401).json({ message: "Access denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+    if (!user) return res.status(401).json({ message: "User not found" });
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
