5d79c334079c2f0bdd88ab379b6bf02682a55784
afreen shaik <affushaik9493@gmail.com>
2025-02-09 02:02:12 +0530

    add a getcurrent user method

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const { authenticate } = require("../middleware/authMiddleware"); // Add this middleware
+const {
+  register,
+  login,
+  getCurrentUser,
+} = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.get("/me", authenticate, getCurrentUser); // Add this route
 module.exports = router;
