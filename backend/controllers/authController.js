6fa9f2061c9ef1f9c56855ebd6d5dbb64bbedcdc
afreen shaik <affushaik9493@gmail.com>
2025-02-09 02:01:16 +0530

    add get current user method

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,3 +37,13 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+// get current user
+exports.getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
