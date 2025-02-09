fa52db89259dbd1a5a2ef605869169e8b69f1378
afreen shaik <affushaik9493@gmail.com>
2025-02-09 20:41:27 +0530

    add frontend url

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,10 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 require("dotenv").config();
-// const CLOUDINARY_CLOUD_NAME = "dznf57les";
-// const CLOUDINARY_API_KEY = "417954382843869";
-// const CLOUDINARY_API_SECRET = "JhIz9aJj5ll5JY6OnHyleriqj9g";
-// Cloudinary Config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
